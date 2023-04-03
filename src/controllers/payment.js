b705214a9e66eb2f950982e8a5ac9fb00752ef83
Tripatjot Singh <101708792+tripat1803@users.noreply.github.com>
2023-04-03 14:17:10 +0530

    Update payment.js

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -83,7 +83,7 @@ export const verification = async (req, res) => {
                 products: order,
                 shippingPrice: details.items[0].notes.shippingPrice,
                 taxPrice: details.items[0].notes.taxPrice,
-                totalPrice: details.amount,
+                totalPrice: details.items[0].amount,
                 shippingId: {
                     mobile: shipping.mobile,
                     address: shipping.address,
