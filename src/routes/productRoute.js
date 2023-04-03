51051d86b532c9c154832c956bac9d86261a1b24
Tripatjot Singh <101708792+tripat1803@users.noreply.github.com>
2023-04-03 11:18:47 +0530

    Update productRoute.js

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProduct, deleteProduct, deleteProductImage, getAllProduct, getCategory, getProduct, getProductByCategory, uploadProductImage } from "../controllers/product.js";
+import { createProduct, deleteProduct, deleteProductImage, getAllCategoryProducts, getAllProduct, getCategory, getProduct, getProductByCategory, uploadProductImage } from "../controllers/product.js";
 import { verifyAdmin } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -12,5 +12,6 @@ router.route("/create").post(verifyAdmin, createProduct);
 router.route("/upload").post(verifyAdmin, uploadProductImage);
 router.route("/").post(verifyAdmin, deleteProduct);
 router.route("/delete/:public_id").post(verifyAdmin, deleteProductImage);
+router.route("/group").get(getAllCategoryProducts);
 
 export default router;
